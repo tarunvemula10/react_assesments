8fdd756808a8982602914af1cd9dff59fa6aa89f
tharunvemula10 <tarun.vemula2445@gmail.com>
2024-05-09 20:51:20 +0530

    updated

--- task8/src/App.jsx
+++ task8/src/App.jsx
@@ -1,15 +1,26 @@
 import axios from 'axios';
 import './App.css'
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  const api = axios.create({
-    baseURL : `http://localhost:8080/data`
-  })
-  api.get('/getall').then(res => console.log(res.data))
+  const [data, setData] = useState([])
+
+  useEffect(()=> {
+    axios.get('http://localhost:8080/data/getall')
+    .then(res => setData(res.data))
+    .catch(err => console.log(err))
+    console.log(data)
+  }, [])
 
   return (
-    <div></div>
+    <div>
+      <ul>
+        {data.map((element,index)=> {
+          <li key={index}>{`${element.name}, ${element.marks}`}</li>
+        })}
+      </ul>
+    </div>
   )
 }
 
